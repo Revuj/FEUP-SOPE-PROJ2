d06d5fea5cb0d440583ccc89689eeb21580e6178
JoAPSo Rafael Gomes Varela <up201706072@feup-b208d10.fe.up.pt>
2019-05-08 16:02:30 +0100

    Created bank offices

---
+++ server.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+
+#include "sope.h"
+#include "types.h"
+#include "constants.h"
+#include "log.c"
+
+int main(int argc, char **argv)
+{
+    if (argc != 3 )
+    {
+        printf("Usage: ./server <number of bank offices> <admin password>");
+        return 1;
+    }
+
+    int bank_offices_no = atoi(argv[1]);
+
+    bank_account_t admin_account;
+    admin_account.account_id = ADMIN_ACCOUNT_ID;
+    admin_account.balance = ADMIN_ACCOUNT_BALLANCE;
+    
+
+    logAccountCreation(STDOUT_FILENO, ADMIN_ACCOUNT_ID, &admin_account);
+
+    for (int i = 1; i <= bank_offices_no; i++) {
+         logBankOfficeOpen(STDOUT_FILENO, i, i);
+    }
+
+
+    for (int i = 1; i <= bank_offices_no; i++) {
+         logBankOfficeClose(STDOUT_FILENO, i, i);
+    }
+}
