6a77c1ff30d0f3d0939e30abbbf2b9ed8c92ff9f
JoaoAbelha <44231794+JoaoAbelha@users.noreply.github.com>
2019-05-08 17:35:01 +0100

    coffe time

--- user.c
+++ user.c
@@ -11,72 +11,63 @@
 #define LENGTH_NAME 20
 
 static char hexa[15] = {"123456789abcdef"};
-
-char *generateRandomSal(char *sal)
+void generateRandomSal(char *sal)
 {
 
     for (int i = 1; i <= SALT_LEN; i++)
     {
         strcat(sal, &hexa[rand() % 15]);
     }
-
-    return sal;
 }
-
-
 
 int main(int argc, char *argv[]) // USER //ID SENHA ATRASO DE OP OP(NR) STRING
 {
-    
-    
-    int fd = open(SERVER_FIFO_PATH,0660);
-    if (fd < 0) {
+
+    int fd = open(SERVER_FIFO_PATH, 0660);
+    if (fd < 0)
+    {
         exit(1);
     }
 
-    if (strcmp(argv[1],"0") == 0) {
-
+    if (strcmp(argv[1], "0") == 0)
+    {
     }
-
 
     tlv_request_t tlv;
     tlv.type = atoi(argv[4]);
-    
 
     //fill header
     tlv.value.header.pid = getpid();
     tlv.value.header.account_id = atoi(argv[1]);
-    strcpy(tlv.value.header.password,argv[2]);
+    strcpy(tlv.value.header.password, argv[2]);
     tlv.value.header.op_delay_ms = atoi(argv[3]);
 
-    
-
     uint32_t id;
-    
 
-    switch(tlv.type) {
-        case OP_CREATE_ACCOUNT:
-        id = atoi(strtok(argv[5]," "));
+    switch (tlv.type)
+    {
+    case OP_CREATE_ACCOUNT:
+        id = atoi(strtok(argv[5], " "));
         tlv.value.create.account_id = id;
-        uint32_t balance= atoi(strtok(argv[5]," "));
+        uint32_t balance = atoi(strtok(argv[5], " "));
         tlv.value.create.balance = balance;
         strcpy(tlv.value.create.password, argv[5]);
         //tlv.length = 13;
         break;
-        case OP_BALANCE:
+    case OP_BALANCE:
         break;
-        case OP_TRANSFER:
-        id = atoi(strtok(argv[5]," "));
+    case OP_TRANSFER:
+        id = atoi(strtok(argv[5], " "));
         tlv.value.transfer.account_id = id;
         tlv.value.transfer.amount = atoi(argv[5]);
         break;
-        case OP_SHUTDOWN:
+    case OP_SHUTDOWN:
         break;
-        
-
     }
 
+    write(fd, &tlv, sizeof(tlv));
 
+    unlink(fifoName);
     // srand(time(NULL));
     // pid_t pid = getpid();
     // char fifoName[20] = USER_FIFO_PATH_PREFIX;
@@ -84,11 +75,10 @@ int main(int argc, char *argv[]) // USER //ID SENHA ATRASO DE OP OP(NR) STRING
 
     // if (strcmp(argv[1],"0") == 0)
     // {
-        
+
     //     account.account_id = atoi(argv[1]);
     //     account.balance = atoi(argv[2]);
     // }
-
 
     // if (strcat(fifoName, atoi(pid)) == NULL)
     // {
@@ -98,6 +88,4 @@ int main(int argc, char *argv[]) // USER //ID SENHA ATRASO DE OP OP(NR) STRING
     // {
     //     return 1;
     // }
-
-    // unlink(fifoName);
 }
